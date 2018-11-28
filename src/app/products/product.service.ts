df0b730500f6209f191f922f0ed762542b0bd180
Ersen Başaran Şen <ersen.sen@gmail.com>
2018-11-28 20:23:24 +0300

    012. Navigation and Routing Additional Techniques

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,37 +1,44 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+
 import { IProduct } from './product';
-import { Observable, ObservableInput, throwError } from "rxjs";
-import { tap, catchError } from "rxjs/operators";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ProductService {
-    private productUrl = 'api/products/products.json';
-    constructor(private http: HttpClient) {}
+  private productUrl = 'api/products/products.json';
 
-    getProducts() : Observable<IProduct[]> {
-        return this.http.get<IProduct[]>(this.productUrl).pipe(
-          tap(data => console.log('All: ' + JSON.stringify(data))),
-          catchError(this.handleError)
-        );
-    }
+  constructor(private http: HttpClient) { }
 
-    private handleError(err: HttpErrorResponse, caugt: Observable<IProduct[]>) : ObservableInput<IProduct[]> {
-      // in a real world app, we may send the server to some remote logging infrastructore
-      // instead of just logging it to the console
-      let errorMessage = '';
-      if(err.error instanceof ErrorEvent){
-        // A client-side or network error occured. Handle it accordingly.
-        errorMessage = `An error occured: ${err.error.message}`;        
-      }
-      else {
-        // The backend returned an unsuccessful response code.
-        // The response body may contain clues as to what went wrong.
-        errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
-      }
-      console.log(errorMessage);
-      return throwError(errorMessage);
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  getProduct(id: number): Observable<IProduct | undefined> {
+    return this.getProducts().pipe(
+      map((products: IProduct[]) => products.find(p => p.productId === id))
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
     }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 }
